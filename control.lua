bc9f501ec9f3773a8ed37fa1a1ad7d1f4f345859
Caesar2011 <slim03@gmx.de>
2023-08-24 19:14:46 +0200

    Improved code

--- control.lua
+++ control.lua
@@ -1,6 +1,5 @@
----@type fun(event: CustomInputEvent)
-
-local function validate_player(player)
+---@param player LuaPlayer
+local function validatePlayer(player)
     if not player then
         return false
     end
@@ -19,90 +18,80 @@ local function validate_player(player)
     if not player.crafting_queue_size then
         return false
     end
-    return true
+    return player.crafting_queue_size > 0
 end
 
-local function promoteCraft(event)
-    local player = game.players[event.player_index]
-    if not validate_player(player) and player.crafting_queue_size == 0 then
-        return nil
-    end
-
+---@param player LuaPlayer
+local function cancelCraftingQueue(player)
     ---@type CraftingQueueItem[]
     local crafts = {}
-    local index = 1
+    local index = 0
     while player.crafting_queue_size > 0 do
+        index = index + 1
         local last_craft = player.crafting_queue[player.crafting_queue_size]
         crafts[index] = last_craft
         player.cancel_crafting{index=last_craft.index, count=last_craft.count}
-        index = index + 1
     end
-    index = index - 1
+    return crafts
+end
 
-    local next_craft = crafts[1]
-    player.begin_crafting{count=next_craft.count, recipe=next_craft.recipe, silent=False}
+---@param player LuaPlayer
+---@param craft CraftingQueueItem
+local function beginCrafting(player, craft)
+    player.begin_crafting{count=craft.count, recipe=craft.recipe, silent=false}
+end
 
+---@param event CustomInputEvent
+function promoteCraft(event)
+    local player = game.players[event.player_index]
+    if not validatePlayer(player) then
+        return nil
+    end
 
-    for remake_index = index, 2, -1 do
-        next_craft = crafts[remake_index]
-        player.begin_crafting{count=next_craft.count, recipe=next_craft.recipe, silent=False}
+    local crafts = cancelCraftingQueue(player)
+    local length = #crafts
+
+    beginCrafting(player, crafts[1])
+    for i = length, 2, -1 do
+        beginCrafting(player, crafts[i])
     end
 end
-script.on_event("promote-craft", promoteCraft)
+script.on_event("promote-craft", --[[---@type]] promoteCraft)
 
 
----@type fun(event: CustomInputEvent)
+---@param event CustomInputEvent
 local function demoteCraft(event)
     local player = game.players[event.player_index]
-    if not validate_player(player) and player.crafting_queue_size == 0 then
+    if not validatePlayer(player) then
         return nil
     end
 
-    ---@type CraftingQueueItem[]
-    local crafts = {}
-    local index = 1
-    while player.crafting_queue_size > 0 do
-        local last_craft = player.crafting_queue[player.crafting_queue_size]
-        crafts[index] = last_craft
-        player.cancel_crafting{index=last_craft.index, count=last_craft.count}
-        index = index + 1
-    end
-    index = index - 1
+    local crafts = cancelCraftingQueue(player)
+    local length = #crafts
 
-    for remake_index = index-1, 1, -1 do
-        local next_craft = crafts[remake_index]
-        player.begin_crafting{count=next_craft.count, recipe=next_craft.recipe, silent=False}
+    for i = length-1, 1, -1 do
+        beginCrafting(player, crafts[i])
     end
-    next_craft = crafts[index]
-    player.begin_crafting{count=next_craft.count, recipe=next_craft.recipe, silent=False}
+    beginCrafting(player, crafts[length])
 
 end
-script.on_event("demote-craft", demoteCraft)
+script.on_event("demote-craft", --[[---@type]] demoteCraft)
 
 
----@type fun(event: CustomInputEvent)
+---@param event CustomInputEvent
 local function resetCraft(event)
     local player = game.players[event.player_index]
-    if not validate_player(player) and player.crafting_queue_size == 0 then
+    if not validatePlayer(player) then
         return nil
     end
 
+    local crafts = cancelCraftingQueue(player)
+    local length = #crafts
 
-    local crafts = {}
-    local index = 1
-    while player.crafting_queue_size > 0 do
-        local last_craft = player.crafting_queue[player.crafting_queue_size]
-        crafts[index] = last_craft
-        player.cancel_crafting{index=last_craft.index, count=last_craft.count}
-        index = index + 1
-    end
-    index = index - 1
-
-    for remake_index = index, 1, -1 do
-        next_craft = crafts[remake_index]
-        player.begin_crafting{count=next_craft.count, recipe=next_craft.recipe, silent=False}
+    for i = length, 1, -1 do
+        beginCrafting(player, crafts[i])
     end
 
 end
-script.on_event("reset-craft", resetCraft)
+script.on_event("reset-craft", --[[---@type]] resetCraft)
 
